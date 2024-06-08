c89d1bc0b7d8c68f9d33716d3561e331bf33aa2e
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-08 16:09:55 +0200

    [MODIFED] task-01.py: added update_user method, created_at and updated at attributes, more testing

--- task-01.py
+++ task-01.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import uuid
+import datetime
 
 class User:
     user_count = 0
@@ -13,6 +14,8 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.id = uuid.uuid1()
+        self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.today()
         User.user_count += 1
     
     def get_user(self):
@@ -27,6 +30,15 @@ class User:
 
     def __del__(self):
         User.user_count -= 1
+    
+    def update_user(self, dictionary):
+        for key, value in dictionary.items():
+            for keys, values in self.__dict__.items():
+                if key == keys:
+                    self.__dict__[key] = value
+        self.updated_at = datetime.datetime.today()
+        return self.__dict__
+
 
 
 class Review:
@@ -48,4 +60,11 @@ class Review:
 my_user = User(name="Sofiane", host=True, reviewer=False, email="my_email@mail.com", password="1234")
 print(my_user.get_user())
 print(my_user.user_count)
+print("My user attributes before:", my_user.__dict__)
+print("My user object creation date", my_user.created_at)
+print("----------------------------")
+print("my user object updated date", my_user.updated_at)
+print("Changing object attribute:")
+my_user_attributes = my_user.update_user({'name': 'Axel', 'host': False, 'reviewer': True, 'email': 'my_other_email@mail.com', 'password': 'ABCD'})
 
+print("My user attributes:", my_user_attributes)
