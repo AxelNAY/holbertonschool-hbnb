fb5891cd86f8375588a9212505fa207d8c381900
AxelNAY <axelnay@sfr.fr>
2024-06-10 12:01:18 +0200

    Update the files

--- amenities.py
+++ amenities.py
@@ -2,12 +2,29 @@ import uuid
 import Place from place
 
 class Ameneties:
+    amneties = ["couch", "dishwasher", "fridge", "microwave", "wifi", "TV", "Balcony"]
     def __init__(self, id_feature, name):
         self.id_place = uuid.uuid4()
         self.name = name
+        self.created_at = datetime.datetime.today()
 
     def add_amenities(self, name):
-        pass
+        my_dict = {}
+        j = 0
+        self.created_at = datetime.datetime.today()
+        for key in amneties:
+            if name == amneties[i]:
+                my_dict[j] = self_name
+                j += 1
+
+    def update_amenities(self, name):
+        my_dict = {}
+        j = 0
+        self.update_at = datetime.datetime.today()
+        for key in amneties:
+            if name == amneties[i]:
+                my_dict[j] = self_name
+                j += 1
 
     def get_amenities(self, name):
         pass
--- place.py
+++ place.py
@@ -1,32 +1,63 @@
 import uuid
+import datetime
+import User from user
 import City from city
 import Country from country
 import Amenities from amenities
 import Reviews from reviews
 
 class Place:
-    def __init__(self, id_place, name, description, address, id_city, id_country, latitude, longitude, id_user, rooms_number, bathrooms, price_night, max_guest, id_feature, id_reviews):
-        self.id_place = uuid.uuid4()
+    def __init__(self, name="", description="", adress="", latitude=0, longitude=0, host=None, rooms=0,
+                 bathrooms=0, price_night=0, guest_capacity=0, city_name="", country_name="", first_name="", last_name=""):
+        super().__init__(city_name=city_name, country_name=country_name, first_name=first_name, last_name=last_name)
         self.name = name
         self.description = description
-        self.address = address
-        self.id_city = id_city
-        self.id_country = id_country
+        self.adress = adress
         self.latitude = latitude
         self.longitude = longitude
-        self.id_user = id_user
-        self.rooms_number = rooms_number
+        self.rooms = rooms
         self.bathrooms = bathrooms
         self.price_night = price_night
-        self.max_guest = max_guest
-        self.id_feature = id_feature
-        self.id_reviews = id_reviews
+        self.guest_capacity = guest_capacity
+        self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.today()
+        self.id_place = uuid.uuid1()
 
-    def add_place(self, name, description, address, id_city, id_country, latitude, longitude, id_user, rooms_number, bathrooms, price_night, max_guest, id_feature, id_reviews):
-        pass
+    def add_place(self, name, description, address, latitude, longitude, rooms, bathrooms,
+            price_night, guest_capacity, city_name, country_name, first_name, last_name):
+        my_dict = {}
+        self.created_at = datetime.datetime.today()
+        my_dict.update({'name': name})
+        my_dict.update({'description': description})
+        my_dict.update({'address': address})
+        my_dict.update({'latitude': latitude})
+        my_dict.update({'longitude': longitude})
+        my_dict.update({'rooms': rooms})
+        my_dict.update({'price_night': price_night})
+        my_dict.update({'guest_capacity': guest_capacity})
+        my_dict.update({'city_name': city_name})
+        my_dict.update({'country_name': country_name})
+        my_dict.update({'first_name': first_name})
+        my_dict.update({'last_name': last_name})
+        return my_dict
 
-    def update_place(self, name, description, address, id_city, id_country, latitude, longitude, id_user, rooms_number, bathrooms, price_night, max_guest, id_feature, id_reviews):
-        pass
+    def update_place(self, name, description, address, latitude, longitude, rooms, bathrooms,
+            price_night, guest_capacity, city_name, country_name, first_name, last_name):
+        my_dict = {}
+        self.updated_at = datetime.datetime.today()
+        my_dict.update({'name': name})
+        my_dict.update({'description': description})
+        my_dict.update({'address': address})
+        my_dict.update({'latitude': latitude})
+        my_dict.update({'longitude': longitude})
+        my_dict.update({'rooms': rooms})
+        my_dict.update({'price_night': price_night})
+        my_dict.update({'guest_capacity': guest_capacity})
+        my_dict.update({'city_name': city_name})
+        my_dict.update({'country_name': country_name})
+        my_dict.update({'first_name': first_name})
+        my_dict.update({'last_name': last_name})
+        return my_dict
 
     def get_place(self, name, description, address, id_city, id_country, latitude, longitude, id_user, rooms_number, bathrooms, price_night, max_guest, id_feature, id_reviews):
         pass
