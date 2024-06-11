7308fbaba313b8272d6323c1d0f899d2cb473602
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-11 14:00:39 +0200

    [ADDED] Implemented save methods in Review and User classes, it works.

--- Review.py
+++ Review.py
@@ -7,19 +7,30 @@ class Review:
     review_count = 0
     feedback = {}
     def __init__(self, rating=0.0, feedback=""):
-        self.id = uuid.uuid1()
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
-        Review.review_count_count += 1
+        self.__id = uuid.uuid1()
+        self.__created_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
+        Review.review_count += 1
 
     def __del__(self):
         Review.review_count -= 1
     
     def update(self, rating=0.0, feedback="" ):
-        self.updated_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
         Review.feedback.update({'rating': rating})
         Review.feedback.update({'feedback': feedback })
 
-    def save(self, object):
-        with open("objects.json", 'w') as myFile:
-            json.dump(object, myFile)
+    def save(self):
+        my_dict_save = {}
+        for key, values in self.__dict__.items():
+            if key == "_Review__created_at" or key == "_Review__updated_at" or key == "_Review__id":
+                my_dict_save.update({key: str(values)})
+            else:
+                my_dict_save.update({key: values})
+        with open("Saving_files/Review.json", 'w') as myFile:
+            json.dump(my_dict_save, myFile)
+
+
+my_review = Review(10.0, "Best place ever")
+print(my_review.__dict__)
+my_review.save()
--- User.py
+++ User.py
@@ -8,15 +8,15 @@ class User:
     user_list = []
     def __init__(self, email="", password="",
                 first_name="", last_name="", status=""):
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.status = status
-        self.places_owned = []
-        self.id = uuid.uuid1()
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+        self.__email = email
+        self.__password = password
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__status = status
+        self.__places_owned = []
+        self.__id = uuid.uuid1()
+        self.__created_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
         User.user_count += 1   
 
     def __del__(self):
@@ -34,6 +34,17 @@ class User:
         user = f"{self.first_name}, {self.last_name}"
         return user
 
-    def save(self, object):
-        with open("User.json", 'w') as myFile:
-            json.dump(object, myFile)
+    def save(self):
+        my_dict_save = {}
+        for key, values in self.__dict__.items():
+            if key == "_User__created_at" or key == "_User__updated_at" or key == "_User__id":
+                my_dict_save.update({key: str(values)})
+            else:
+                my_dict_save.update({key: values})
+        with open("Saving_files/User.json", 'w') as myFile:
+            json.dump(my_dict_save, myFile)
+
+
+my_user = User("Random@", "ABCD", "Sofiane", "Slimane", "host")
+print(my_user.__dict__)
+my_user.save()
