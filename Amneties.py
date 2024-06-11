89650fea08e1c41d70ff1724c165c2bf608764be
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-11 13:33:27 +0200

    [MODIFIED] Implemented save function for City, it works

--- Amneties.py
+++ Amneties.py
@@ -4,20 +4,24 @@ import datetime
 import json
 
 class Amneties:
-    amneties_list = ["couch", "dishwasher", "fridge", "microwave", "wifi", "TV", "Balcony"]
+    
     amnetie_count = 0
     def __init__(self):
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
-        self.id = uuid.uuid1()
+        self.__amneties = ["couch", "dishwasher", "fridge", "microwave", "wifi", "TV", "Balcony"]
+        self.__created_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
+        self.__id = uuid.uuid1()
         Amneties.amnetie_count += 1
     
     def __del__(self):
         Amneties.amnetie_count -= 1
-    
+    @property
     def get(self):
-        return Amneties.amneties_list
-    
+        return self.__amneties
+    @get.setter
+    def get(self, amnetie=""):
+        self.__amneties.append(amnetie)
+        self.__updated_at = datetime.datetime.today()
 
     def update(self, new_amnetie=""):
         Amneties.amneties_list.append(new_amnetie)
--- City.py
+++ City.py
@@ -9,14 +9,31 @@ class City(Country):
     city_list = []
     city_count = 0
     def __init__(self, city_name=""):
-        self.city_name = city_name
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
-        self.id = uuid.uuid1()
+        self.__city_name = city_name
+        self.__created_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
+        self.__id = uuid.uuid1()
         City.city_count += 1
-    
+    @property
     def get(self):
-        return self.city_name
+        return self.__city_name
+    @get.setter
+    def get(self, city_name=""):
+        self.__city_name = city_name
+    
+
+
+    @property
+    def get_update_date(self):
+        return self.__updated_at
+    
+    @property
+    def get_creation_date(self):
+        return self.__created_at
+    
+    @property
+    def get_id(self):
+        return self.__id
     
     def update(self, new_city):
         self.city_name = new_city 
@@ -25,6 +42,16 @@ class City(Country):
     def __del__(self):
         City.city_count -= 1
     
-    def save(self, object):
-        with open("City.json", 'w') as myFile:
-            json.dump(object, myFile)
+    def save(self):
+        my_dict_save = {}
+        for key, values in self.__dict__.items():
+            if key == "_City__created_at" or key == "_City__updated_at" or key == "_City__id":
+                my_dict_save.update({key: str(values)})
+            else:
+                my_dict_save.update({key: values})
+
+        with open("Saving_files/City.json", 'w') as myFile:
+            json.dump(my_dict_save, myFile)
+
+my_city = City("Bordeaux")
+my_city.save()
