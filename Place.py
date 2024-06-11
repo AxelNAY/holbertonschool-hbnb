07f6e5e5187b38840d53ad0c5362345dd6e74200
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-11 13:49:47 +0200

    [MODIFIED] Implemented save function for Place, it works

--- Place.py
+++ Place.py
@@ -9,37 +9,49 @@ class Place:
 
     def __init__(self, name="", description="", adress="", latitude=0, longitude=0, rooms=0,
                  bathrooms=0, price_night=0, guest_capacity=0, city_name=""):
-        self.name = name
-        self.description = description
-        self.adress = adress
-        self.latitude = latitude
-        self.longitude = longitude
-        self.rooms = rooms
-        self.bathrooms = bathrooms
-        self.price_night = price_night
-        self.guest_capacity = guest_capacity
-        self.city_name = city_name
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
-        self.id = uuid.uuid1()
+        self.__name = name
+        self.__description = description
+        self.__adress = adress
+        self.__latitude = latitude
+        self.__longitude = longitude
+        self.__rooms = rooms
+        self.__bathrooms = bathrooms
+        self.__price_night = price_night
+        self.__guest_capacity = guest_capacity
+        self.__city_name = city_name
+        self.__created_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
+        self.__id = uuid.uuid1()
         Place.place_count += 1
 
-
+    @property
     def get(self):
-            return self.name
+            return self.__name
     
 
 
-    def save(self, object):
-        with open("objects.json", 'w') as myFile:
-            json.dump(object, myFile)
+    def save(self):
+        my_dict_save = {}
+        for key, values in self.__dict__.items():
+            if key == "_Place__created_at" or key == "_Place__updated_at" or key == "_Place__id":
+                my_dict_save.update({key: str(values)})
+            else:
+                my_dict_save.update({key: values})
+
+        with open("Saving_files/Place.json", 'w') as myFile:
+            json.dump(my_dict_save, myFile)
         
     def update(self, dictionary):
         for key, value in dictionary.items():
             for keys, values in self.__dict__.items():
                 if key == keys:
                     self.__dict__[key] = value
-        self.updated_at = datetime.datetime.today()
+        self.__updated_at = datetime.datetime.today()
     
     def __del__(self):
         Place.place_count -= 1
+    
+
+my_place = Place("HBNB", "No Wifi", "Holberton street", 0, 0, 5, 2, 150, 10, "Bordeaux")
+print(my_place.__dict__)
+my_place.save()
