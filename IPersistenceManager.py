8f352bda14f48002e31215f079d3a1febedd541e
AxelNAY <axelnay@sfr.fr>
2024-06-11 15:25:22 +0200

    Complete the file IPersistenceManager.py

--- IPersistenceManager.py
+++ IPersistenceManager.py
@@ -17,56 +17,16 @@ class IPersistenceManager(ABC):
 
     @abstractmethod
     def save(self, entity):
-        with open("entity.json", 'w') as my_file:
-            json.dump(entity, my_file)
+        pass
 
     @abstractmethod
     def get(self, entity_id, entity_type):
-        if entity_type == Place:
-            return self.get_place(entity_id)
-        elif entity_type == User:
-            return self.get_user(entity_id)
-        elif entity_type == City:
-            return self.get_city(entity_id)
-        elif entity_type == Amenities:
-            return self.get_amenities(entity_id)
-        elif entity_type == Review:
-            return self.get_review(entity_id)
-        elif entity_type == Country:
-            return self.get_country(entity_id)
-        else:
-            raise TypeError("Type d'entité non pris en charge")
+        pass
 
     @abstractmethod
     def update(self, entity):
-        if isinstance(entity, Place):
-            self.update_place(entity)
-        elif isinstance(entity, User):
-            self.update_user(entity)
-        elif isinstance(entity, City):
-            self.update_city(entity)
-        elif isinstance(entity, Amenities):
-            self.update_amenities(entity)
-        elif isinstance(entity, Review):
-            self.update_review(entity)
-        elif isinstance(entity, Country):
-            self.update_country(entity)
-        else:
-            raise TypeError("Type d'entité non pris en charge")
+        pass
 
     @abstractmethod
     def delete(self, entity_id, entity_type):
-        if entity_type == Place:
-            self.delete_place(entity_id)
-        elif entity_type == User:
-            self.delete_user(entity_id)
-        elif entity_type == City:
-            self.delete_city(entity_id)
-        elif entity_type == Amenities:
-            self.delete_amenities(entity_id)
-        elif entity_type == Review:
-            self.delete_review(entity_id)
-        elif entity_type == Country:
-            self.delete_country(entity_id)
-        else:
-            raise TypeError("Type d'entité non pris en charge")
+        pass
