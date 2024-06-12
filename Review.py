3086494e690ee73748edfda24fa3d9ff1a5dbe82
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-12 14:15:13 +0200

    [ADDED] Review.py: Implemented delete() method

--- Review.py
+++ Review.py
@@ -33,6 +33,11 @@ class Review:
 
 
     def delete(self):
+        for dictionary in Review.review_object_list:
+            if dictionary['_Review__id'] == self.__id:
+                 Review.review_object_list.remove(dictionary)
+        with open("Saving_files/Review.json", 'w') as myFile:
+            json.dump(Review.review_object_list, myFile, indent=4)
         Review.review_count -= 1
     
     def update(self, rating=0.0, feedback="" ):
@@ -42,9 +47,17 @@ class Review:
     def save(self):
         Review.review_object_list.append(self.__dict__)
         with open("Saving_files/Review.json", 'w') as myFile:
-            json.dump(Review.review_object_list, myFile)
+            json.dump(Review.review_object_list, myFile, indent=4)
 
 
 my_review = Review(10.0, "Best place ever")
+my_review2 = Review(5.0, "Not bad")
+my_review3 = Review(0.0, "Never coming back")
 print(my_review.__dict__)
 my_review.save()
+my_review2.save()
+my_review3.save()
+
+my_review2.delete()
+my_review.delete()
+my_review3.delete()
