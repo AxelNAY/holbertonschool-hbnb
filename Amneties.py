f6e77b060e0691386ff4d723f15dc4c12162dfa8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-06-12 14:57:44 +0200

    [ADDED] Amneties.py: implemented delete() method

--- Amneties.py
+++ Amneties.py
@@ -6,15 +6,13 @@ import json
 class Amneties:
     
     amnetie_count = 0
+    amneties_object_list = []
     def __init__(self):
         self.amneties = ["couch", "dishwasher", "fridge", "microwave", "wifi", "TV", "Balcony"]
         self.created_at = str(datetime.datetime.today())
         self.updated_at = str(datetime.datetime.today())
         self.__id = str(uuid.uuid1())
         Amneties.amnetie_count += 1
-    
-    def __del__(self):
-        Amneties.amnetie_count -= 1
     
     @property
     def get(self):
@@ -24,7 +22,12 @@ class Amneties:
         self.__id = value
         self.updated_at = str(datetime.datetime.today())
     
-    
+    def delete(self):
+        for dictionary in Amneties.amneties_object_list:
+            if dictionary['_Amneties__id'] == self.__id:
+                 Amneties.amneties_object_list.remove(dictionary)
+        with open("Saving_files/Amneties.json", 'w') as myFile:
+            json.dump(Amneties.amneties_object_list, myFile, indent=4)
 
     def update(self, new_amnetie=""):
         self.amneties.append(new_amnetie)
@@ -32,15 +35,26 @@ class Amneties:
     
     
     def save(self):
-         with open("Saving_files/Amneties.json", 'a') as myFile:
-            json.dump(self.__dict__, myFile)
+        Amneties.amneties_object_list.append(self.__dict__)
+        with open("Saving_files/Amneties.json", 'w') as myFile:
+            json.dump(Amneties.amneties_object_list, myFile, indent=4)
 
         
     
 
 
 my_amneties = Amneties()
+my_amneties2 = Amneties()
+my_amneties2.update("Playstation 4")
+my_amneties3 = Amneties()
+my_amneties3.update("Home Cinema")
 print(my_amneties.__dict__)
-my_amneties.update('Ninento Switch')
-print(my_amneties.__dict__)
+
 my_amneties.save()
+my_amneties2.save()
+my_amneties3.save()
+
+
+my_amneties.delete()
+my_amneties2.delete()
+my_amneties3.delete()
